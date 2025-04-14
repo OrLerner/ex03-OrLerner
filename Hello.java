237b5d61b3970be03dfbb86a182d3e03105097a7
OrLerner <orler1234@gmail.com>
2025-04-14 13:53:46 +0300

    Update Hello.java to print a new message

--- Hello.java
+++ Hello.java
@@ -1,1 +1,9 @@
-public class Hello{ public static void main(String[] args) { for (int i=1;i<=10; i++) { System.out.println("Hello World ..."+i); } } } 
+public class Hello{ 
+    public static void main(String[] args) 
+    { 
+        for (int i=1;i<=10; i++) 
+        { 
+            System.out.println("Welcome to Hello.java file"+i); 
+        } 
+    } 
+} 
